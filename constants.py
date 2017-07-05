68cfb0ff7197f86f644cec8f8f6bb0105048329d
lakshay rajput <shawnrajput@gmail.com>
2017-07-05 21:22:52 +0530

    generated token properly & got my info from insta (Self_info.py)

--- constants.py
+++ constants.py
@@ -2,5 +2,5 @@
 import requests
 
 
-App_Access_Token = "d7a15ea6a60a471a9d83d216f54f78a9"
+App_Access_Token = "2118945007.d7a15ea.71f34869f8e14e1994df7b76d3f450f0"
 Base_url = "https://api.instagram.com/v1/"
